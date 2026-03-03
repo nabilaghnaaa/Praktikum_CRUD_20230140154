00610b4d17dc23fca2fadca8f31210cbf54ace20
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2026-03-03 11:55:57 +0700

    membuat UserService

--- src/main/java/com/deploy/praktikum1/service/UserService.java
+++ src/main/java/com/deploy/praktikum1/service/UserService.java
@@ -2,13 +2,17 @@ package com.deploy.praktikum1.service;
 
 import com.deploy.praktikum1.model.dto.UserAddRequest;
 import com.deploy.praktikum1.model.dto.UserDto;
-
 import java.util.List;
 
-public class UserService {
-    UserDto AddUser(UserAddRequest request);
+public interface UserService {
+
+    UserDto addUser(UserAddRequest request);
+
     List<UserDto> getAllUser();
+
     UserDto getUserById(String id);
-    UserDto UpdateUser(String id, UserAddRequest request);
-    void DeleteUser(String id);
+
+    UserDto updateUser(String id, UserAddRequest request);
+
+    void deleteUser(String id);
 }
